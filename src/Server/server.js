b7b1a1225bb47679cfe8cc8a9a704ca753cfff99
CaseyWav <casey@waverka.com>
2023-09-22 20:24:58 -0400

    adding of npm sql and editing of server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,5 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql =  require('mysql');
+
+app.use(cors());
 
 app.get("/api", (req,res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
